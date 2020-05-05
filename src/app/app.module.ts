d6ef6aa4a5cf9151ef5aeb53a0d210712b8e09e7
himanshisingh1304 <himanshisingh0413@gmail.com>
2020-05-05 13:10:39 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { SuccessmsgComponent } from './successmsg/successmsg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    SignupComponent,
+    SuccessmsgComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/successmsg/successmsg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successmsg',
+  templateUrl: './successmsg.component.html',
+  styleUrls: ['./successmsg.component.css']
+})
+export class SuccessmsgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
